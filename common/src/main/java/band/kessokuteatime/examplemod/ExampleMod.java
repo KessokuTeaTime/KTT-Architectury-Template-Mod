3e33c28db5cdf0251d57fb07e0dd8c0dd68355b6
KrLite <68179735+KrLite@users.noreply.github.com>
2023-09-22 21:54:48 +0800

    come on

--- common/src/main/java/band/kessokuteatime/examplemod/ExampleMod.java
+++ common/src/main/java/band/kessokuteatime/examplemod/ExampleMod.java
@@ -7,7 +7,6 @@ import dev.architectury.registry.registries.RegistrarManager;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
@@ -15,16 +14,21 @@ import net.minecraft.util.Identifier;
 import java.util.function.Supplier;
 
 public class ExampleMod {
-    public static final String MOD_ID = "examplemod";
+    public static final String ID = "examplemod", NAME = "Example Mod";
+
     // We can use this if we don't want to use DeferredRegister
-    public static final Supplier<RegistrarManager> REGISTRIES = Suppliers.memoize(() -> RegistrarManager.get(MOD_ID));
-    // Registering a new creative tab
-    public static final ItemGroup EXAMPLE_TAB = CreativeTabRegistry.create(Text.translatable(new Identifier(MOD_ID, "example_tab").toTranslationKey()), () ->
-            new ItemStack(ExampleMod.EXAMPLE_ITEM.get()));
+    public static final Supplier<RegistrarManager> REGISTRIES = Suppliers.memoize(() -> RegistrarManager.get(ID));
+
+    public static final ItemGroup EXAMPLE_TAB = CreativeTabRegistry.create(
+            Text.translatable(new Identifier(ID, "example_tab").toTranslationKey()),
+            () -> ExampleMod.EXAMPLE_ITEM.get().getDefaultStack()
+    );
     
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MOD_ID, RegistryKeys.ITEM);
-    public static final RegistrySupplier<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () ->
-            new Item(new Item.Settings().arch$tab(ExampleMod.EXAMPLE_TAB)));
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ID, RegistryKeys.ITEM);
+    public static final RegistrySupplier<Item> EXAMPLE_ITEM = ITEMS.register(
+            "example_item",
+            () -> new Item(new Item.Settings().arch$tab(ExampleMod.EXAMPLE_TAB))
+    );
     
     public static void init() {
         ITEMS.register();
--- forge/src/main/java/band/kessokuteatime/examplemod/forge/ExampleModForge.java
+++ forge/src/main/java/band/kessokuteatime/examplemod/forge/ExampleModForge.java
@@ -5,11 +5,11 @@ import dev.architectury.platform.forge.EventBuses;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(ExampleMod.MOD_ID)
+@Mod(ExampleMod.ID)
 public class ExampleModForge {
     public ExampleModForge() {
         // Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(ExampleMod.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+        EventBuses.registerModEventBus(ExampleMod.ID, FMLJavaModLoadingContext.get().getModEventBus());
         ExampleMod.init();
     }
 }
